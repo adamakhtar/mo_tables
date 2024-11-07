6b507ca138a5cc8f730f5f2406b307825408692d
Adam Akhtar <adamsubscribe@googlemail.com>
2024-11-07 23:42:06 +0000

    add first commit

---
+++ lib/mo_tables.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mo_tables/version"
+
+module MoTables
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/mo_tables/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module MoTables
+  VERSION = "0.1.0"
+end
